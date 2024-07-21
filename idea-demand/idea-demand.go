9449e2827d773693e9a3f3eadbacd7adc60c66c9
AleYepes <73545779+AleYepes@users.noreply.github.com>
2024-07-21 19:58:23 +0200

    Begin wallapop scraper

--- idea-demand/idea-demand.go
+++ idea-demand/idea-demand.go
@@ -2,9 +2,23 @@ package main
 
 import (
 	"fmt"
-	"math"
 )
 
-func main(){
-	fmt.Println(math.Abs(-4))
+type Saiyan struct{
+	Name string
+	Power int
+	Father *Saiyan
+}
+
+func main() {
+	goku := &Saiyan{
+		Name: "Goku", 
+		Power: 9000,
+		Father: nil} 
+	Super(goku) 
+	fmt.Println(goku.Father)
+}
+
+func Super(s *Saiyan) { 
+	s.Power += 10000
 }
